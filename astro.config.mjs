2577762d60cf48a56505f816f5539669843482d4
Joe Chegne <joe@cooties.io>
2025-12-10 15:26:20 +1000

    removed placeholder blogs and added first blogpost!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://cooties.io',
   integrations: [mdx()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'github-light',
       wrap: false,
